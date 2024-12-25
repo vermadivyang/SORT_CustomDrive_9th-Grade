09adb9149563991af929eac89283b29cc720c25f
Divyang Verma <divyang.verma26@gmail.com>
2024-12-25 14:53:39 -0500

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,9 @@
+import field_coords
+
+# Example usage
+field_coords.startPos(0, 0, 0)
+field_coords.move_to_pivot_forward(1, 1)
+field_coords.move_to_tank_forward(2, 2)
+field_coords.move_to_pivot_backward(1, 0)
+field_coords.move_to_tank_backward(0, 0)
+field_coords.end()
