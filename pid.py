4baa0656b2642c56a970caaf5ccf2da7ac97ec27
Divyang Verma <divyang.verma26@gmail.com>
2024-12-25 14:37:26 -0500

    Update pid.py

--- pid.py
+++ pid.py
@@ -1,1 +1,16 @@
+class PID:
+    def __init__(self, Kp, Ki, Kd, setpoint):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.setpoint = setpoint
+        self.prev_error = 0
+        self.integral = 0
+
+    def compute(self, current_value):
+        error = self.setpoint - current_value
+        self.integral += error
+        derivative = error - self.prev_error
+        self.prev_error = error
+        return self.Kp * error + self.Ki * self.integral + self.Kd * derivative
 
